d98126898fae12d3f18a5f425938fd39f8ed1e6c
gjg95 <101204553+gjg95@users.noreply.github.com>
2022-03-08 17:45:14 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import dash
+import sqlite3
+
+
+
+conn = sqlite3.connect(
+    'C:/Users/jsolana/Desktop/Planta aguas Project/Data/DATABASE.db',
+    check_same_thread=False
+)
+cur = conn.cursor()
+
+# meta_tags are required for the app layout to be mobile responsive
+app = dash.Dash(__name__, suppress_callback_exceptions=True,
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}
+                ]
+)
+
+server = app.server
+
+print("APP READY, dashboard visualización planta de aguas") 
